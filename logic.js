e33075e6c7a2c64a3ab2c3fd157e28f68d7f06a1
Om Patil <omajitsinhpatil@gmail.com>
2023-01-14 00:03:56 +0530

    cards

--- logic.js
+++ logic.js
@@ -1,100 +1,134 @@
-
+// async functi
 var object = {
-    status: "ok",
+
     data: [
         {
             title: "lorem1",
-            description: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
+            score: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
             source: "https://www.youtube.com/"
         }, {
             title: "twittwer",
-            description: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
+            score: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
             source: "https://www.youtube.com/"
         }, {
             title: "google",
-            description: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
+            score: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
             source: "https://www.youtube.com/"
         }, {
             title: "facebook",
-            description: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
+            score: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
             source: "https://www.youtube.com/"
         },
         {
             title: "facebook",
-            description: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
+            score: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
             source: "https://www.youtube.com/"
         }, {
             title: "facebook",
-            description: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
+            score: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
             source: "https://www.youtube.com/"
         }, {
             title: "facebook",
-            description: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
+            score: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
             source: "https://www.youtube.com/"
         }, {
             title: "facebook",
-            description: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
+            score: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
             source: "https://www.youtube.com/"
         }, {
             title: "facebook",
-            description: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
+            score: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
             source: "https://www.youtube.com/"
         }, {
             title: "facebook",
-            description: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
+            score: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
             source: "https://www.youtube.com/"
         }, {
             title: "facebook",
-            description: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
+            score: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
             source: "https://www.youtube.com/"
         }, {
             title: "facebook",
-            description: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
+            score: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
             source: "https://www.youtube.com/"
         }, {
             title: "facebook",
-            description: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
+            score: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
             source: "https://www.youtube.com/"
         }, {
             title: "facebook",
-            description: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
+            score: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
             source: "https://www.youtube.com/"
         },
     ]
 
 }
 
+// fetch("http://127.0.0.1:8000/docs#/default", {
+//     headers: {
+
+//     }
+// })
+// const api_url = 'http://127.0.0.1:8000/articles?name=twitter';
+// async function getUser() {
+
+//     let select = document.getElementsByClassName('c-services');
+//     writehtml(data);
+
+//     async function writehtml(object) {
 
 
-let select = document.getElementsByClassName('c-services');
-writehtml(object);
+//         // Making an API call (request)
+//         // and getting the response back
+//         const response = await fetch(api_url);
 
-async function writehtml(object) {
+//         // Parsing it to JSON format
+//         const data = await response.json();
 
-    if (object.status === "ok") {
-        document.querySelector('.c-services').innerHTML = '';
 
-        for (let i = 0; i < object.data.length; i++) {
-            document.querySelector('.c-services').innerHTML += `
-    
-            <a class="c-services__item" href="${object.data[i].source}">
-            
-            
-        <li ">
-        <h3>${object.data[i].title}</h3>
-        <p>${object.data[i].description}
-        </p>
-    </li></a>`
 
-        }
+//     }
 
-    } else {
-        document.querySelector('.c-services').innerHTML = '';
-        document.querySelector('.c-services').innerHTML += `<h1 class="c-services__item">There was an error loading your result</h1>`
+//     // console.log(data.score);
 
-    }
 
+
+
+
+
+// }
+
+// // Calling the function
+// getUser();
+
+
+document.querySelector('.c-services').innerHTML = '';
+
+for (let i = 0; i < object.data.length; i++) {
+    document.querySelector('.c-services').innerHTML += `
+
+<a class="c-services__item" href="${object.data[i].source}">
+
+
+<li ">
+<h3>${object.data[i].title}</h3>
+<p>${object.data[i].score}
+</p>
+</li></a>`
 
 }
+
+
+
+
+// data = fetch("http://127.0.0.1:8000/docs#/default")
+    // else {
+    //     document.querySelector('.c-services').innerHTML = '';
+    //     document.querySelector('.c-services').innerHTML += `<h1 class="c-services__item">There was an error loading your result</h1>`
+
+    // }
+
+
+
 
 
