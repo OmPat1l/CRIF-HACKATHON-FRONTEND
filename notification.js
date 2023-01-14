0483b1e54931ac67227b1d1ec07548c3ac012b11
Om Patil <omajitsinhpatil@gmail.com>
2023-01-14 10:39:00 +0530

    json try

--- notification.js
+++
@@ -1,9 +0,0 @@
-setTimeout(function () {
-  $(".title").fadeOut("slow", function () {
-    var div = $("<div class='title'>My Dashboard</div>").hide();
-    $(this).replaceWith(div);
-    $(".title").fadeIn("slow");
-  });
-
-  // $(".title").html('My Dashboard');
-}, 1500);
