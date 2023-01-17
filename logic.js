c740fb8e4174fcf0a0674e001793135403f1a8c7
Om Patil <omajitsinhpatil@gmail.com>
2023-01-17 11:34:22 +0530

    trial

---
+++ logic.js
@@ -0,0 +1,121 @@
+// // async functi
+// var result = {
+
+//     detail: [
+//         {
+//             title: "lorem1",
+//             score: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
+//             link: "https://www.youtube.com/"
+//         }, {
+//             title: "twittwer",
+//             score: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
+//             link: "https://www.youtube.com/"
+//         }, {
+//             title: "google",
+//             score: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
+//             link: "https://www.youtube.com/"
+//         }, {
+//             title: "facebook",
+//             score: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
+//             link: "https://www.youtube.com/"
+//         },
+//         {
+//             title: "facebook",
+//             score: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
+//             link: "https://www.youtube.com/"
+//         }, {
+//             title: "facebook",
+//             score: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
+//             link: "https://www.youtube.com/"
+//         }, {
+//             title: "facebook",
+//             score: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
+//             link: "https://www.youtube.com/"
+//         }, {
+//             title: "facebook",
+//             score: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
+//             link: "https://www.youtube.com/"
+//         }, {
+//             title: "facebook",
+//             score: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
+//             link: "https://www.youtube.com/"
+//         }, {
+//             title: "facebook",
+//             score: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
+//             link: "https://www.youtube.com/"
+//         }, {
+//             title: "facebook",
+//             score: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
+//             link: "https://www.youtube.com/"
+//         }, {
+//             title: "facebook",
+//             score: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
+//             link: "https://www.youtube.com/"
+//         }, {
+//             title: "facebook",
+//             score: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
+//             link: "https://www.youtube.com/"
+//         }, {
+//             title: "facebook",
+//             score: "orem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, sed eius consequuntur beatae asperiores deleniti commodi rem aliquam. Nam, beatae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos natus voluptates ipsa eos sapiente officia repellendus ducimus. Aliquam veniam deleniti doloribus nobis aliquid rem suscipit repellat. Dolorum rem aspernatur similique!",
+//             link: "https://www.youtube.com/"
+//         },
+//     ]
+
+// }
+
+
+
+
+
+
+
+
+
+// let resp = global1(result);
+
+
+
+// function global1(prop1){
+//     return prop1;
+
+// // }
+
+
+// console.log(result.detail);
+
+
+// function display_data(result) {
+//     document.querySelector('.c-services').innerHTML = '';
+//     const length = result["detail"].reduce((a, obj) => a + Object.keys(obj).length, 0);
+//     // console.log(result[0])
+
+//     for (let i = 0; i < length; i++) {
+//         document.querySelector('.c-services').innerHTML += `
+
+//     <a class="c-services__item" href="${result["detail"][i].link}">
+
+
+// <li ">
+// <h3>${result["detail"][i]["title"]}</h3>
+// <p>${result["detail"][i]["score"]}
+// </p>
+// </li></a>`
+
+//     }
+// }
+
+
+
+
+// data = fetch("http://127.0.0.1:8000/docs#/default")
+    // else {
+    //     document.querySelector('.c-services').innerHTML = '';
+    //     document.querySelector('.c-services').innerHTML += `<h1 class="c-services__item">There was an error loading your result</h1>`
+
+    // }
+
+
+
+
+
